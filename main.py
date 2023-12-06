2cbcf6a75d61f9b827bca1afdf459269c4508cbc
H. Reza <81297423+hamidreza-mohammadiha@users.noreply.github.com>
2023-12-06 15:53:38 +0330

    Pricebar

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def home():
-    return "home"
+@app.route('/get_price')
+def get_price():
+    api_url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    response = requests.get(api_url)
+    data = response.json()
+    price = data.get('price', 'N/A')
+    return {'price': price}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=2080)
