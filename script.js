f09732bcf625ed25db05485c773b46eb89418082
H. Reza <81297423+hamidreza-mohammadiha@users.noreply.github.com>
2024-02-23 23:31:49 +0330

    e5

--- script.js
+++ script.js
@@ -18,6 +18,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+  
+    document.getElementById('clock').innerText = `${hours}:${minutes}:${seconds}`;
+  }
+  
+  setInterval(updateClock, 1000);
+  
+
   
 
     
