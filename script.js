252ef84b11677ed1096c80e1ae6d9a39e14ea1ab
H. Reza <81297423+hamidreza-mohammadiha@users.noreply.github.com>
2024-02-24 23:36:23 +0330

    e2

--- script.js
+++ script.js
@@ -13,34 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
       
     }
   
-setInterval(updateAge, 1); // Update the age every millisecond
-updateAge(); // Run function immediately to avoid delay
+    setInterval(updateAge, 1); // Update the age every millisecond
+    updateAge(); // Run function immediately to avoid delay
   });
-
-  const numberElement = document.getElementById('number');
-  let currentNumber = 3.5;
-  const maxRotation = 10; // degrees
-  const animationDuration = 3.5; // seconds for half cycle
-  
-  function updateNumber(isIncreasing) {
-    const step = isIncreasing ? 0.1 : -0.1;
-    currentNumber += step;
-  
-    if (currentNumber <= 1 || currentNumber >= 6) {
-      clearInterval(intervalId);
-      setTimeout(() => {
-        intervalId = setInterval(() => updateNumber(!isIncreasing), animationDuration * 1000 / (maxRotation * 2));
-      }, animationDuration * 1000);
-    }
-  
-    numberElement.textContent = currentNumber.toFixed(1);
-  }
-  
-  let intervalId = setInterval(() => updateNumber(true), animationDuration * 1000 / (maxRotation * 2));
-  
-  
-
-  
-
-    
   
