976dd7ef6be97168184562af9a1ceeda3efc8c01
H. Reza <81297423+hamidreza-mohammadiha@users.noreply.github.com>
2024-05-10 23:22:26 +0330

    17

--- Portfolio/script_2.js
+++ Portfolio/script_2.js
@@ -1,11 +1,16 @@
-const link = document.querySelector('a');
+function highlightLink() {
+    const link = document.querySelector('a');
 
-link.addEventListener('mouseover', () => {
-  link.style.color = 'red';
-  link.style.transition = 'color 0.5s ease-in-out';
-});
+    link.addEventListener('mouseover', () => {
+      link.style.color = 'red';
+      link.style.transition = 'color 0.5s ease-in-out';
+    });
 
-link.addEventListener('mouseout', () => {
-  link.style.color = 'blue';
-  link.style.transition = 'color 0.5s ease-in-out';
-});
+    link.addEventListener('mouseout', () => {
+      link.style.color = 'blue';
+      link.style.transition = 'color 0.5s ease-in-out';
+    });
+  }
+
+  // Call the function when the page loads
+  window.addEventListener('DOMContentLoaded', highlightLink);
