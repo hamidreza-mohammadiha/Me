0833d6e0177ce6feb25b62124842ebfcd048edca
H. Reza <81297423+hamidreza-mohammadiha@users.noreply.github.com>
2024-05-10 22:12:06 +0330

    9

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const ageInSeconds = ageInMilliseconds / 1000;
       const ageInYears = ageInSeconds / (60*60*24*365.25)
   
-      document.getElementById('Years').innerText = ageInYears.toFixed(9); // Display age in Years
+      document.getElementById('Years').innerText = ageInYears.toFixed(10); // Display age in Years
       // document.getElementById('Seconds').innerText = ageInSeconds.toFixed(2); // Display age in seconds with three decimals for milliseconds
       
     }
