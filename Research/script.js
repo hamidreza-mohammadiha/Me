8531c75033f0dd1dfb31cf5b9c7712f3dce67e95
H. Reza <81297423+hamidreza-mohammadiha@users.noreply.github.com>
2025-02-10 11:25:11 +0000

    r7

--- Research/script.js
+++ Research/script.js
@@ -4,9 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function updateAge() {
       const now = new Date();
       document.getElementById('time').innerText = now;
-      // document.getElementById('Seconds').innerText = ageInSeconds.toFixed(2); // Display age in seconds with three decimals for milliseconds
     }
   
     setInterval(updateAge, 1000); // Update the age every millisecond
-    updateAge(); // Run function immediately to avoid delay
   });
